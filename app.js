4d9ba31f8a3e56b92371cd0d4ae2a0fe2695980e
axel-ac <abdullahceylan.axel@gmail.com>
2023-02-23 16:33:30 +0300

    fixed

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ btn.addEventListener("click", () => {
       win.textContent = `${y} - ${e}`;
     } else if (x === randomNum) {
       alert("Marvelous");
-      body.style.backgroundImage =
-        "https://media.istockphoto.com/id/1361394182/photo/funny-british-shorthair-cat-portrait-looking-shocked-or-surprised.jpg?b=1&s=170667a&w=0&k=20&c=nOa1R7PGaqOaQscx10FpA5ZNenMeDfs-k6VgmmuY4cc=";
+      body.style.backgroundImage = "url(https://media.istockphoto.com/id/1361394182/photo/funny-british-shorthair-cat-portrait-looking-shocked-or-surprised.jpg?b=1&s=170667a&w=0&k=20&c=nOa1R7PGaqOaQscx10FpA5ZNenMeDfs-k6VgmmuY4cc=)"
+      h3.textContent = `You succeeded! Here's your surprise 😊`;
     }
   } else {
     alert("please enter a number between 1-100");
