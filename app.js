68e3c8abba4741dd9411da8577697b887e66ac03
axel-ac <abdullahceylan.axel@gmail.com>
2023-02-23 03:04:37 +0300

    JS finished

---
+++ app.js
@@ -0,0 +1,50 @@
+const input = document.getElementById("input");
+const btn = document.querySelector(".btn");
+const body = document.querySelector("#body");
+const randomNum = Math.round(Math.random() * 100);
+const img = document.getElementsByTagName("img");
+const win = document.querySelector(".win");
+const span = document.querySelector(".span");
+const refresh = document.getElementById("refresh");
+const h3 = document.getElementById("h3");
+
+let counter = 0;
+let y = 1;
+let e = 100;
+
+input.addEventListener("keydown", (z) => {
+  if (z.key === "Enter") {
+    btn.click();
+  }
+});
+
+btn.addEventListener("click", () => {
+  let x = input.value;
+
+  if (0 < x && x <= 100) {
+    counter++;
+    if (x < 1 || x > 100) {
+      alert("please enter a number between 1-100");
+    } else if (randomNum > x) {
+      y = x;
+      alert("up");
+
+      span.textContent = counter;
+      win.textContent = `${y} - ${e}`;
+    } else if (randomNum < x) {
+      e = x;
+      alert("Down");
+      span.textContent = counter;
+      win.textContent = `${y} - ${e}`;
+    } else if (x === randomNum) {
+      alert("Marvelous");
+      body.style.backgroundImage =
+        "https://cdn.pixabay.com/photo/2023/01/23/15/02/beaver-rat-7738914__340.jpg";
+    }
+  } else {
+    alert("please enter a number between 1-100");
+  }
+  input.focus();
+});
+
+refresh.addEventListener("click", () => window.location.reload());
